e1eadb72fab52104c281cb201477496c93254fdc
Dariusz Grzeszczak <d.grzeszczak@gmail.com>
2019-10-10 21:28:52 +0200

    - ViewModelProvided added

---
+++ ReMVVMExt/Sources/ViewModelProvided.swift
@@ -0,0 +1,30 @@
+//
+//  ViewModelProvided.swift
+//  ReMVVMExt
+//
+//  Created by Dariusz Grzeszczak on 10/10/2019.
+//
+
+import Loaders
+import ReMVVM
+
+@propertyWrapper
+public struct ViewModelProvided<VMD> where VMD: ViewModelDriven {
+
+    public var wrappedValue: VMD? {
+        didSet {
+            guard   let responder = wrappedValue,
+                    let viewModel = viewModel.wrappedValue
+            else { return }
+            responder.viewModel = viewModel
+        }
+    }
+
+    private var viewModel: Provided<VMD.ViewModelType>
+    public init() {
+        viewModel = Provided()
+    }
+    public init(key: String) {
+        viewModel = Provided(key: key)
+    }
+}
